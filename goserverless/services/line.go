a703c253d88733a7ccf1bb5e5a39a804629eece3
kosaku kurino <kousaku.maron@gmail.com>
2019-10-10 11:17:34 +0900

    feat: add parse request func

--- goserverless/services/line.go
+++ goserverless/services/line.go
@@ -1,23 +1,52 @@
 package services
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
 	"os"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 var (
-	channelID     string = os.Getenv("CHANNEL_ID")
-	channelSecret string = os.Getenv("CHANNEL_SECRET")
+	channelAccessToken string = os.Getenv("CHANNEL_ACCESS_TOKEN")
+	channelSecret      string = os.Getenv("CHANNEL_SECRET")
 )
 
 // InitBot function
 func InitBot() (*linebot.Client, error) {
-	bot, err := linebot.New(channelID, channelSecret)
+	bot, err := linebot.New(channelAccessToken, channelSecret)
 
 	if err != nil {
 		return nil, err
 	}
 
 	return bot, nil
+}
+
+// ParseRequest function
+func ParseRequest(r events.APIGatewayProxyRequest) ([]*linebot.Event, error) {
+	if !validateSignature(channelSecret, r.Headers["X-Line-Signature"], []byte(r.Body)) {
+		return nil, linebot.ErrInvalidSignature
+	}
+	request := &struct {
+		Events []*linebot.Event `json:"events"`
+	}{}
+	if err := json.Unmarshal([]byte(r.Body), request); err != nil {
+		return nil, err
+	}
+	return request.Events, nil
+}
+
+func validateSignature(channelSecret, signature string, body []byte) bool {
+	decoded, err := base64.StdEncoding.DecodeString(signature)
+	if err != nil {
+		return false
+	}
+	hash := hmac.New(sha256.New, []byte(channelSecret))
+	hash.Write(body)
+	return hmac.Equal(decoded, hash.Sum(nil))
 }
